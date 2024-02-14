42a30c398009e139bcd905bb7c0a9a8ada59989c
Sheps <shepsalmighty@gmail.com>
2024-02-14 17:57:24 +0000

    Add Password Manager + db.py

---
+++ db.py
@@ -0,0 +1,80 @@
+import sqlite3
+# import pwdMain
+
+db_file = "pwd_Manager_DB.sqlite"
+conn = sqlite3.connect(f"{db_file}")
+
+# creating cursor object - lets us use SQL statements
+cur = conn.cursor()
+
+# creating master log in table for user login info
+create_master_table = cur.execute("CREATE TABLE IF NOT EXISTS master_credentials(username, password)")
+
+#Confirm master user exists in pwdMain to allow users to login otherwise create user and login
+def user_exits(user):
+    sql_query = """SELECT username FROM master_credentials WHERE username = (?)"""
+    values = (user,)
+    cur.execute(sql_query, values)
+    row = cur.fetchone()
+    if row:
+        return True
+    # using else return False to ensure no Null value is returned
+    else:
+        return False
+
+
+def get_password(user):
+    sql_query = """SELECT password FROM master_credentials WHERE username = (?)"""
+    values = (user,)
+    cur.execute(sql_query, values)
+    return cur.fetchone()[0]
+
+# def new_user(user, master_user_pwd):
+def new_user(key1, key2):
+    update_db = """INSERT INTO master_credentials (username, password) VALUES (?, ?) """
+    new_user_values = (key1, key2)
+    cur.execute(update_db, new_user_values)
+    conn.commit()
+
+# creating the table to input user credtials (will only be called once as once it exists no need to create again)
+create_table = cur.execute("""CREATE TABLE IF NOT EXISTS credentials(website, username, password)""")
+
+
+res = cur.execute("""SELECT website, username, password FROM credentials""")
+
+# data fetches the first line/row from credentials table
+# data = res.fetchone()
+# website, username, password = data
+# print(f"'{website}', '{username}', '{password}'")
+
+# inserting values into the table
+sql_query = """INSERT INTO credentials(website, username, password) VALUES (?, ?, ?)"""
+values = ('twitch', 'username', 'password')  # Replace these with the actual values you want to insert
+cur.execute(sql_query, values)
+conn.commit()
+
+# turning the above into a function
+def write_to_db(get_site, get_login, get_pwd):
+    sql_query = """INSERT INTO credentials(website, username, password) VALUES (?, ?, ?)"""
+    # values = (str(get_site), str(get_login), str(get_pwd))
+    values = (key1, key2, key3)
+    cur.execute(sql_query, values)
+    conn.commit()
+
+# table got cluttered from constant testing so this func clears all
+# def cleanUp():
+#     clean = """DELETE FROM credentials;"""
+#     cur.execute(clean)
+#     conn.commit()
+
+
+# testing if we ran the program multiple times causing the f-string to have too many objects
+# for row in data:
+#     print(row)
+
+
+
+# if __name__ == '__main__':
+#     create_connection(r"C:\Users\Sheps (Inverted PC)\Desktop\code\pwd_Manager_DB.sqlite")
+
+# Using https://docs.python.org/3/library/sqlite3.html
---
+++ pwdMain.py
@@ -0,0 +1,206 @@
+ #  Password generator/ manager
+ # 1) Ask user if they want to store or retrieve a password
+ # 2) for store ask what for (google, website, etc) - then take their username/pwd and store in json or csv file
+ # 3) if retrieving a pwd ask you ask them for what service and then you print all of the users and password with that,
+# because you can have multiple saves accounts on one service.
+
+# Bonus tasks:
+# 1) Make a master password, which you'll have to enter in order to access the choice to either storw or get a password.
+# So if a user enters incorrect master password you don't let them in.
+# 2) Encrypt the passwords before saving and then decrypt them before printing them out, when the user requests them.
+# https://stackoverflow.com/questions/90413/what-is-the-best-easiest-to-use-encryption-library-in-python
+# 3) Use database to store passwords '
+# 4) Add user ability to delete credentials (CRUD maybe, please god hope you remember what this means)
+
+import cryptography
+from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+import db
+import os
+import sqlite3
+
+
+
+ # import db  #we need to write the functions we'll want to call on the data base in the db file
+
+FILENAME = "add_creds.json"
+
+
+def derive(param):
+    salt = b'\xa66k\xd6)\xe1\xef\xc4)\x1d\nl\xc3I\x19\xa5'
+    kdf = Scrypt(salt=salt, length=32, n=2**14, r=8, p=1)
+
+    encrypt = kdf.derive(param.encode("utf-8"))
+    return encrypt
+
+
+def verify(param, user):
+    salt = b'\xa66k\xd6)\xe1\xef\xc4)\x1d\nl\xc3I\x19\xa5'
+    kdf = Scrypt(salt=salt, length=32, n=2 ** 14, r=8, p=1)
+
+    kdf.verify(param.encode("utf-8"), user)
+
+
+#
+# //User should input their username and pwd to gain access to all of their stored credentials IF NONE EXITS ALREADY
+def set_master_credentials():
+    # //get user inputs for username + pwd
+    user = input("Input master username: ")
+
+    key1 = derive(user)
+
+    user_check = db.user_exits(key1)
+
+    if user_check:
+        master_user_pwd = input("Login: ")
+        safeword = "gibberish " + master_user_pwd
+        verify(safeword, db.get_password(key1))
+        key = derive(master_user_pwd)
+    else:
+        master_user_pwd = input("Create your master password: ")
+        key = derive(master_user_pwd)
+# making a safeword so we don't store the encryption key in the db
+        safeword = derive("gibberish " + master_user_pwd)
+        db.new_user(key1, safeword)
+    return key
+
+
+
+
+
+# //verifying the user and encrypted password, we'll use this later to allow us into actions
+def verify_master_user():
+    pwdCorrect = set_master_credentials
+    progress = main()
+
+    if pwdCorrect == None:
+        progress
+
+
+
+
+
+
+def encrypt(key,v):
+    iv = os.urandom(16)
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
+    encryptor = cipher.encryptor()
+    encodedValue = v.encode("utf-8")
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(encodedValue)
+    padded_data += padder.finalize()
+    ct = encryptor.update(padded_data) + encryptor.finalize()
+
+    print(len(padded_data))
+    return ct,iv
+
+
+def decrypt(key, iv, ct):
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
+    decryptor = cipher.decryptor()
+    unpadder = padding.PKCS7(128).unpadder()
+    decrypted_text = decryptor.update(ct) + decryptor.finalize()
+    unpadded_text = unpadder.update(decrypted_text) + unpadder.finalize()
+    # decryptor.update(ct)
+
+    return decryptor.update(ct) + decryptor.finalize()
+
+
+
+
+
+
+
+def do_action():
+    action = input("Do you want to Store, Retrieve, or Delete a password? S/R/D. Else Q to Quit: ").upper()
+    return action
+
+
+# //creating an empty dict to interact with the json file
+user_dict = {}
+
+def add_creds():
+       get_site = input("What website is this for?: ").lower()
+
+       get_login = input("enter your username: ")
+
+       get_pwd = input("enter your password: ")
+       # below updated the dict we oringially used but this isn't secure at all
+       # user_dict[get_site] = {"username": get_login,
+       #                         "password": get_pwd}
+
+
+
+       db.write_to_db
+
+       #
+       # salt = b'\xb66k\xd6)\xe1\xef\xc4)\x1d\nl\xc3I\x19\xa5'
+       # kdf1 = Scrypt(salt=salt, length=32, n=2 ** 14, r=8, p=1)
+       # kdf2 = Scrypt(salt=salt, length=32, n=2 ** 14, r=8, p=1)
+       # kdf3 = Scrypt(salt=salt, length=32, n=2 ** 14, r=8, p=1)
+       #
+       # key1 = kdf1.derive(get_site.encode("utf-8"))
+       # key2 = kdf2.derive(get_login.encode("utf-8"))
+       # key3 = kdf3.derive(get_pwd.encode("utf-8"))
+       #(key1, key2, key3)
+       #
+       # kdf_verify1 = Scrypt(salt=salt, length=32, n=2 ** 14, r=8, p=1)
+       # kdf_verify2 = Scrypt(salt=salt, length=32, n=2 ** 14, r=8, p=1)
+       # kdf_verify3 = Scrypt(salt=salt, length=32, n=2 ** 14, r=8, p=1)
+       # verify_key_1 = kdf_verify1.verify(get_site.encode("utf-8"), key1)
+       # verify_key_2 = kdf_verify2.verify(get_login.encode("utf-8"), key2)
+       # verify_key_3 = kdf_verify3.verify(get_pwd.encode("utf-8"), key3)
+
+
+
+
+
+#// user interaction options for storing/retrieving/deleting etc credentials
+def main():
+    while True:
+        action = do_action()
+        if action == "S":
+            add_creds()
+        elif action == "R":
+            get_creds()
+        # elif action == "D":
+        #     delete_credentials()
+        elif action == "Q":
+            break  # Exit the loop
+        else:
+            print("Invalid action. Please choose S(tore), R(etrieve), D(elete), or Q(uit).")
+
+
+
+
+# print(encrypt(derive("222"), "test"))
+print(decrypt(derive("222"), b'e\x8d8\x9a,\x8b+;j\x13\xcd$D\xc6\xbbC', b'\xf6\xb1\xb8\x9a!\x14\x05<?B\xc5\xaf0\xb8\xf0x'))
+set_master_credentials()
+verify_master_user()
+encrypt()
+# main()
+# add_creds()
+# continue_action()
+
+# write_data_json()
+# # read_creds = read_user_json()
+#
+# print(read_creds)
+
+# get_creds(do_action())
+
+
+# if __name__ == "__main__":
+#     main()
+
+
+# PASSWORD HASHING WITH BCRPYT: https://www.geeksforgeeks.org/hashing-passwords-in-python-with-bcrypt/
+# this seems to just encrypt the password and then check a user input is the same by encrypting that and seeing if both
+# encrypted versions match. We need to encrypt and then decrypt the stored password. bcrypt may not be the best option
+# for this, needs some research.
+# this link may be the shiznit https://www.geeksforgeeks.org/how-to-encrypt-and-decrypt-strings-in-python/
+
+
+# Turnas said "what i'd do is AES-256 encryption for the passwords, and use a key derivation function on the master password to derive the key"
+# this project uses a cool ascii art for the terminal interface https://github.com/Abhijeetbyte/MYPmanager/blob/main/main.py
